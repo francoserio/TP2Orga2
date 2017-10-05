4f4cd8d3ca5a0ad31f47701f1e050fbf8f07fa28
franco <francoagustinserio@gmail.com>
2017-10-05 16:08:03 -0300

    sin diff en las v pero seg fault

--- solver.asm
+++ solver.asm
@@ -1,3 +1,18 @@
+extern free
+extern malloc
+
+section .data
+
+ceroCinco: dd 0.5, 0.5, 0.5, 0.5
+negativos: DD -1.0, -1.0, -1.0, -1.0
+andprimeros4: DD 0x00000000, 0x00000000, 0x00000000, 0xFFFFFFFF
+andsegundos4: DD 0x00000000, 0x00000000, 0xFFFFFFFF, 0x00000000
+andterceros4: DD 0x00000000, 0xFFFFFFFF, 0x00000000, 0x00000000
+andcuartos4: DD 0xFFFFFFFF, 0x00000000, 0x00000000, 0x00000000
+primeroNegativo: DD -1.0, 1.0, 1.0, 1.0
+
+section .text
+
 %define offset_fluid_solver_N 0
 %define offset_fluid_solver_dt 4
 %define offset_fluid_solver_diff 8
@@ -9,16 +24,7 @@
 %define offset_fluid_solver_dens 48
 %define offset_fluid_solver_dens_prev 56
 
-section .data 
-ceroCinco: dd 0.5, 0.5, 0.5, 0.5
-negativosSeg: dd 1.0, 1.0, -1.0, -1.0
-negativosPrim: dd -1.0, -1.0, 1.0, 1.0
-
-;global solver_lin_solve
-solver_lin_solve:
-	
-
-;global solver_set_bnd 		;solver en rdi, b en esi, x en rdx
+global solver_set_bnd	;solver en rdi, b en esi, x en rdx
 solver_set_bnd:
 	push rbp				;alineada
 	mov rsp, rbp
@@ -29,274 +35,262 @@ solver_set_bnd:
 	push r15				;desalineada
 	sub rsp, 8				;alineada
 
-	pxor xmm3, xmm3
-	movdqu xmm3, [ceroCinco]	;xmm3 = | 0.5 | 0.5 | 0.5 | 0.5 |
-	mov r15, rdx			;r15 = x
-	mov ebx, [rdi + offset_fluid_solver_N]		;ebx = N
-	mov r9d, ebx+2				; r9d = N + 2
-	mov r8d, ebx+1				; r8d = N + 1
+	xor rbx, rbx
+	xor r15, r15
+	xor r14, r14
+	xor r13, r13
+	xor r12, r12
+	xor r11, r11
+	xor r10, r10
+	xor r9, r9
+	xor r8, r8
+	;pongo en 0 rbx, r14, r13, r12, r11, r10, r9, r8
 
-	;primer paso
-	pxor xmm6, xmm6				;xmm6 = | 0 | 0 | 0 | 0 |
-	pxor xmm7, xmm7				;xmm7 = | 0 | 0 | 0 | 0 |
-
-	mov r11d, [r15 + 1]			;r11d = x[IX(1, 0)]
-	insertps xmm6, r11d, 0		;xmm6 = | 0 | 0 | 0 | x[IX(1, 0)] |
-	mov r11d, [r15 + r9d*r8d + 1];r11d = x[IX(1, N+1)]
-	insertps xmm6, r11d, 32		;xmm6 = | 0 | 0 | x[IX(1, N+1)] | x[IX(1, 0)] |
-	mov r11d, [r15 + ebx]		;r11d = x[IX(N, 0)]
-	insertps xmm6, r11d, 64		;xmm6 = | 0 | x[IX(N, 0)] | x[IX(N, i)] | x[IX(1, i)] |
-	mov r11d, [r15 + r9d*r8d + ebx];r11d = x[IX(N, N+1)]
-	insertps xmm6, r11d, 96		;xmm6 = | x[IX(N, N+1)] | x[IX(i, 1)] | x[IX(N, i)] | x[IX(1, i)] 
-
-	mulps xmm6, xmm3			;xmm6 = | 0.5*x[IX(N, N+1)] | 0.5*x[IX(i, 1)] | 0.5*x[IX(N, i)] | 0.5*x[IX(1, i)] 
-
-	mov r11d, [r15 + r9d]			;r11d = x[IX(0, 1)]
-	insertps xmm7, r11d, 0			;xmm7 = | 0 | 0 | 0 | x[IX(0, 1)] |
-	mov r11d, [r15 + r9d*ebx]		;r11d = x[IX(0, N)]
-	insertps xmm7, r11d, 32			;xmm7 = | 0 | 0 | x[IX(0, N)] | x[IX(1, 0)] |
-	mov r11d, [r15 + r9d + r8d]		;r11d = x[IX(N+1, 1)]
-	insertps xmm7, r11d, 64			;xmm7 = | 0 | x[IX(N+1, 1)] | x[IX(0, N)] | x[IX(1, 0)] |
-	mov r11d, [r15 + r9d*ebx + r8d]	;r11d = x[IX(N+1, N)]
-	insertps xmm7, r11d, 96			;xmm7 = | x[IX(N+1, N)] | x[IX(N+1, 1)] | x[IX(0, N)] | x[IX(1, 0)] |
-
-	addps xmm6, xmm7				;xmm6 = | 0.5*x[IX(N, N+1)] + x[IX(N+1, N)] | 0.5*x[IX(i, 1)] + x[IX(N+1, 1)] | 0.5*x[IX(N, i)] + x[IX(0, N)] | 0.5*x[IX(1, i)]  + x[IX(1, 0)] |
-
-	extractps [r15], xmm6, 0
-	extractps [r15 + r9d*r8d], xmm6, 32
-	extractps [r15 + r8d], xmm6, 64
-	extractps [r15 + r9d*r8d + r8d], xmm6, 96
-
-	;fin del primer paso (que en el codigo de la catedra esta a lo ultimo de la funcion)
-	
-	mov r13d, esi			;r13d = b
+	pxor xmm11, xmm11
 	pxor xmm4, xmm4
-	pxor xmm5, xmm5
-	xor r12d, r12d			;r12 = 0
-	inc r12d							;r12 = 1
-	movdqu xmm4, [negativosSeg]			; xmm4 = | -1.0 | -1.0 | 1.0 | 1.0 |
-	movdqu xmm5, [negativosPrim]		; xmm5 = | 1.0 | 1.0 | -1.0 | -1.0 |
-	cmp r13d, 1					
+	movups xmm11, [ceroCinco]	;xmm11 = | 0.5 | 0.5 | 0.5 | 0.5 |
+	movdqu xmm4, [negativos]									;xmm4 = | -1.0 | -1.0 | -1.0 | -1.0 |
+
+	mov r15, rdx															;r15 = x
+
+	mov ebx, [rdi + offset_fluid_solver_N]		;rbx = N
+	mov r9d, ebx
+	dec r9																		;r9 = N - 1
+	mov r8, r15
+	mov r13, rsi															;r13 = b
+
+	pxor xmm1, xmm1
+	mov r12, rbx
+	shr r12, 2																;r12 = r12 / 4
+	cmp r13, 1
 	je .ciclo1					;b == 1
-	cmp r13d, 2
-	je .ciclo2
-	jmp .fin					;b == 2	
-.ciclo1:						;b == 1
-	xor r11d, r11d
-	cmp r12d, ebx
-	je .fin
-	pxor xmm0, xmm0				; | 0 | 0 | 0 | 0 |
-	pxor xmm1, xmm1 			; | 0 | 0 | 0 | 0 |
-	mov r9d, ebx+2				; r9d = N + 2
-	mov r8d, ebx+1				; r8d = N + 1
-	mov r11d, [r15 + r9d*r12d + 1]	;r11d = x[IX(1,i)]
-	insertps xmm0, r11d, 0		;xmm0 = | 0 | 0 | 0 | x[IX(1,i)] |
-	mov r11d, [r15 + r9d*r12d + ebx];r11d = x[IX(N,i)]
-	insertps xmm0, r11d, 32		;xmm0 = | 0 | 0 | x[IX(N,i)] | x[IX(1,i)] |
-	mov r11d, [r15 + r9d + r12d]	;r11d = x[IX(i,1)]
-	insertps xmm0, r11d, 64		;xmm0 = | 0 | x[IX(i,1)] | x[IX(N,i)] | x[IX(1,i)] |
-	mov r11d, [r15 + r9d*ebx + r12d];r11d = x[IX(i,N)]
-	insertps xmm0, r11d, 96		;xmm0 = | x[IX(i,N)] | x[IX(i,1)] | x[IX(N,i)] | x[IX(1,i)] 
-	;movdqu xmm1, xmm0			;xmm1 = | x[IX(i,N)] | x[IX(i,1)] | x[IX(N,i)] | x[IX(1,i)] |
-	mulps xmm0, xmm5			;xmm0 = | x[IX(i,N)] | x[IX(i,1)] | -x[IX(N,i)] | -x[IX(1,i)] |
-
-	extractps [r15 + r9d*ebx], xmm0, 0
-	extractps [r15 + r9d*ebx + r8d], xmm0, 32
-	extractps [r15 + ebx], xmm0, 64
-	extractps [r15 + r9d*r8d + ebx], xmm0, 96
-
-	inc r12d
+	cmp r13, 2
+	je .ciclo2					;b == 2
+.ciclo1:							;b == 1
+	cmp r12, r11
+	je .finCiclo1
+	;ahora x[IX(i,0  )] = b==2 ? -x[IX(i,1)] : x[IX(i,1)];
+	pxor xmm0, xmm0
+	mov r14, r15
+	mov r10, r15
+	lea r15, [r15 + rbx*4]
+	movdqu xmm0, [r15]				;| x[IX(i+3, 1)] | x[IX(i+2, 1)] | x[IX(i+1, 1)] | x[IX(i, 1)] |
+	mov r15, r14
+	movdqu [r15], xmm0
+	mov r15, r14
+	;x[IX(i,N+1)] = b==2 ? -x[IX(i,N)] : x[IX(i,N)];	;ultima fila
+	xor rax, rax
+	mov rax, r9
+	mul ebx
+	shl rax, 32
+	shrd rax,rdx,32
+	lea r15, [r15 + rax*4]		;voy a la anteultima fila
+	pxor xmm0, xmm0
+	mov r14, r15
+	movdqu xmm0, [r15]				;| x[IX(i+3, N)] | x[IX(i+2, N)] | x[IX(i+1, N)] | x[IX(i, N)] |
+	mov r15, r14
+	lea r15, [r15 + rbx*4]
+	movdqu [r15], xmm0
+	mov r15, r10
+	lea r15, [r15 + 16]
+	inc r11
 	jmp .ciclo1
-
 .ciclo2:						;b == 2
-	xor r11d, r11d
-	cmp r12d, ebx
-	je .fin
-	pxor xmm0, xmm0				; | 0 | 0 | 0 | 0 |
-	pxor xmm1, xmm1 			; | 0 | 0 | 0 | 0 |
-	mov r9d, ebx+2				; r9d = N + 2
-	mov r8d, ebx+1				; r8d = N + 1
-	mov r11d, [r15 + r9d*r12d + 1]	;r11d = x[IX(1,i)]
-	insertps xmm0, r11d, 0		;xmm0 = | 0 | 0 | 0 | x[IX(1,i)] |
-	mov r11d, [r15 + r9d*r12d + ebx];r11d = x[IX(N,i)]
-	insertps xmm0, r11d, 32		;xmm0 = | 0 | 0 | x[IX(N,i)] | x[IX(1,i)] |
-	mov r11d, [r15 + r9d + r12d]	;r11d = x[IX(i,1)]
-	insertps xmm0, r11d, 64		;xmm0 = | 0 | x[IX(i,1)] | x[IX(N,i)] | x[IX(1,i)] |
-	mov r11d, [r15 + r9d*ebx + r12d];r11d = x[IX(i,N)]
-	insertps xmm0, r11d, 96		;xmm0 = | x[IX(i,N)] | x[IX(i,1)] | x[IX(N,i)] | x[IX(1,i)] 
-	;movdqu xmm1, xmm0			;xmm1 = | x[IX(i,N)] | x[IX(i,1)] | x[IX(N,i)] | x[IX(1,i)] |
-	mulps xmm0, xmm4			;xmm1 = | -x[IX(i,N)] | -x[IX(i,1)] | x[IX(N,i)] | x[IX(1,i)] |
-
-	extractps [r15 + r9d*ebx], xmm0, 0
-	extractps [r15 + r9d*ebx + r8d], xmm0, 32
-	extractps [r15 + ebx], xmm0, 64
-	extractps [r15 + r9d*r8d + ebx], xmm0, 96
-
-	inc r12d
+	cmp r12, r11
+	je .finCiclo2
+	;x[IX(i,0  )] = b==2 ? -x[IX(i,1)] : x[IX(i,1)];
+	pxor xmm0, xmm0
+	mov r14, r15
+	mov r10, r15
+	lea r15, [r15 + rbx*4]
+	movdqu xmm0, [r15]				;| x[IX(i+3, 1)] | x[IX(i+2, 1)] | x[IX(i+1, 1)] | x[IX(i, 1)]
+	mulps xmm0, xmm4					;| -x[IX(i+3, 1)] | -x[IX(i+2, 1)] | -x[IX(i+1, 1)] | -x[IX(i, 1)] |
+	mov r15, r14
+	movdqu [r15], xmm0
+	mov r15, r14
+	;x[IX(i,N+1)] = b==2 ? -x[IX(i,N)] : x[IX(i,N)];	;ultima fila
+	xor rax, rax
+	mov rax, r9						;r11 = r8
+	mul ebx								;r11 = r8*r9
+	shl rax,32
+	shrd rax,rdx,32
+	lea r15, [r15 + rax*4]		;estoy en la anteultima fila
+	pxor xmm0, xmm0
+	mov r14, r15
+	movdqu xmm0, [r15]				;| x[IX(i+3, 1)] | x[IX(i+2, 1)] | x[IX(i+1, 1)] | x[IX(i, 1)] |
+	mov r15, r14
+	lea r15, [r15 + rbx*4]
+	movdqu [r15], xmm0
+	mov r15, r10
+	lea r15, [r15 + 16]
+	inc r11
 	jmp .ciclo2
-
+.finCiclo1:
+	xor r13, r13
+	mov r15, r8
+.primeraColumnaCiclo1:
+	cmp r13, rbx
+	je .ultimaColumnaCiclo1
+	mov r11, r15
+	lea r15, [r15 + 4]
+	mov r12, [r15]
+	xor rax, rax
+	mov rax, -1
+	mul r12
+	mov [r11], r12
+	lea r15, [r15 + rbx*4]
+	inc r13
+	jmp .primeraColumnaCiclo1
+.finCiclo2:
+	xor r13, r13
+	mov r15, r8
+.primeraColumnaCiclo2:
+	cmp r13, rbx
+	je .ultimaColumnaCiclo2
+	mov r11, r15
+	lea r15, [r15 + 4]
+	mov r12, [r15]
+	mov [r11], r12
+	lea r15, [r15 + rbx*4]
+	inc r13
+	jmp .primeraColumnaCiclo2
+.ultimaColumnaCiclo1:
+	xor r13, r13
+	mov r15, r8
+.cicloUltimaColumna1:
+	cmp r13, rbx
+	je .fin
+	lea r15, [r15 + r9*4]
+	mov r12, [r15]
+	lea r15, [r15 + 4]
+	xor rax, rax
+	mov rax, -1
+	mul r12
+	mov [r15], r12
+	inc r13
+	lea r15, [r15 + rbx*4]
+	jmp .cicloUltimaColumna1
+.ultimaColumnaCiclo2:
+	xor r13, r13
+	mov r15, r8
+.cicloUltimaColumna2:
+	cmp r13, rbx
+	je .fin
+	lea r15, [r15 + r9*4]
+	mov r12, [r15]
+	lea r15, [r15 + 4]
+	mov [r15], r12
+	lea r15, [r15 + rbx*4]
+	inc r13
+	jmp .cicloUltimaColumna2
 .fin:
+	pxor xmm0, xmm0
+	pxor xmm1, xmm1
+
+	mov r15, r8
+	lea r15, [r15 + 4]
+	mov r14, [r15]			;x[IX(1,0  )]
+	vpinsrq xmm0, r14, 0
+
+	mov r15, r8
+	lea r15, [r15 + rbx*4]
+	mov r14, [r15]			;x[IX(0  ,1)]
+	vpinsrq xmm1, r14, 0
+
+	mov r15, r8
+	xor rax, rax
+	mov rax, rbx
+	mul rbx
+	shl rax,32
+	shrd rax,rdx,32
+	lea r15, [r15 + rax*4]
+	mov r14, [r15]			;x[IX(0  ,N)]
+	vpinsrq xmm1, r14, 32
+
+	mov r15, r8
+	xor rax, rax
+	mov rax, rbx
+	inc r9
+	inc r9
+	mul r9
+	shl rax,32
+	shrd rax,rdx,32
+	lea r15, [r15 + rax*4 + 4]
+	mov r14, [r15]			;x[IX(1,N+1)]
+	vpinsrq xmm0, r14, 32
+
+	mov r15, r8
+	lea r15, [r15 + rbx*4]
+	mov r14, [r15]			;x[IX(N,0  )]
+	vpinsrq xmm0, r14, 64
+
+	mov r15, r8
+	lea r15, [r15 + rbx*4]
+	lea r15, [r15 + r9*4]
+	mov r14, [r15]			;x[IX(N+1,1)]
+	vpinsrq xmm1, r14, 64
+
+	mov r15, r8
+	xor rax, rax
+	mov rax, rbx
+	mul r9
+	shl rax,32
+	shrd rax,rdx,32
+	lea r15, [r15 + rax*4]
+	lea r15, [r15 + rbx*4]
+	mov r14, [r15]			;x[IX(N,N+1)]
+	vpinsrq xmm0, r14, 96
+
+	mov r15, r8
+	xor rax, rax
+	mov rax, rbx
+	mul rbx
+	shl rax,32
+	shrd rax,rdx,32
+	lea r15, [r15 + rax*4]
+	lea r15, [r15 + r9*4]
+	mov r14, [r15]			;x[IX(N+1,N)])
+	vpinsrq xmm1, r14, 96
+
+	mulps xmm0, xmm11
+	mulps xmm1, xmm11
+	addps xmm0, xmm1
+
+	mov r15, r8
+	extractps r14, xmm0, 0
+	mov [r15], r14
+
+	mov r15, r8
+	xor rax, rax
+	mov rax, rbx
+	mul r9
+	shl rax,32
+	shrd rax,rdx,32
+	lea r15, [r15 + rax*4]
+	extractps r14, xmm0, 32
+	mov [r15], r14
+
+	mov r15, r8
+	lea r15, [r15 + r9*4]
+	extractps r14, xmm0, 64
+	mov [r15], r14
+
+	mov r15, r8
+	xor rax, rax
+	mov rax, rbx
+	mul r9
+	shl rax,32
+	shrd rax,rdx,32
+	lea r15, [r15 + rax*4]
+	lea r15, [r15 + r9*4]
+	extractps r14, xmm0, 96
+	mov [r15], r14
+
 	add rsp, 8
 	pop r15
 	pop r14
 	pop r13
 	pop r12
-	pop rbx
-	pop rbp
-	ret
-
-;global solver_project
-solver_project:
-
-	push rbp				;alineada
-	mov rsp, rbp
-	push rbx				;desalineada
-	push r13				;alineada
-	push r14				;desalineada
-	push r15				;alineada
-
-
-	mov r15, rdi ; solver
-	mov r14, rsi ; p
-	mov r13, rdx ; div
-
-	pxor xmm3, xmm3
-	movdqu xmm3, [ceroCinco]	;xmm3 = | 0.5 | 0.5 | 0.5 | 0.5 |
-	
-	pxor xmm4, xmm4
-	movdqu xmm4, [r15 + offset_fluid_solver_N]
-	movdqu xmm5, xmm4
-	pslldq xmm5, 4
-	paddusb xmm4, xmm5
-	pslldq xmm5, 4
-	paddusb xmm4, xmm5
-	pslldq xmm5, 4
-	paddusb xmm4, xmm5 ;xmm3 = | N | N | N | N |
-
-	pxor xmm5, xmm5
-	pxor xmm6, xmm6
-	pxor xmm7, xmm7
-	pxor xmm8, xmm8
-	; FOR_EACH_CELL
-	; 	div[IX(i,j)] = -0.5f * (solver->u[IX(i+1,j)] - solver->u[IX(i-1,j)] + solver->v[IX(i,j+1)] - solver->v[IX(i,j-1)]) / solver->N;
-	; 	p[IX(i,j)] = 0;
-	; END_FOR
-
-	; Siempre agarro 4 conjuntos de 4 pixels, arriba actual abajo, siguiente
-	; comienzo de fila
-	; b b b b - - - - - - - - 
-	; a a a a d d d d - - - -
-	; c c c c - - - - - - - -
-	; - - - - - - - - - - - -
-	; c - b
-	; ciclo
-	; - - - - x x x x - - - - 
-	; x x x x a a a a x x x x
-	; - - - - x x x x - - - -
-	; - - - - - - - - - - - -
-	; fin de fila
-	; - - - - - - - - x x x x
-	; - - - - x x x x a a a a
-	; - - - - - - - - x x x x
-	; - - - - - - - - - - - -
-	; La unica diferencia es que aca opero con todo junto
-
-	.primerCiclo:
-	.principioFilaPC:
-
-	.cicloPC:
-
-	.finFilaPC:
-
-	jmp .primerCiclo
-
-
-	; 	solver_set_bnd ( solver, 0, div ); 
-	mov rdi, r15
-	mov rsi, 0
-	mov rdx, r13
-	call solver_set_bnd
-	; 	solver_set_bnd ( solver, 0, p );
-	mov rdi, r15
-	mov rsi, 0
-	mov rdx, r14
-	; 	solver_lin_solve ( solver, 0, p, div, 1, 4 );
-	mov rdi, r15
-	mov rsi, 0
-	mov rdx, r14
-	mov rcx, r13
-	mov r8, 1
-	mov r9, 4
-	call solver_lin_solve
-
-	; FOR_EACH_CELL
-	
-	; 	solver->u[IX(i,j)] -= 0.5f * solver->N * (p[IX(i+1,j)] - p[IX(i-1,j)]);
-	; mascaras:
-	; a = 0.5 0.5 0.5 0.5
-	; b = solver->N solver->N solver->N solver->N
-	; a * b
-	; 	solver->v[IX(i,j)] -= 0.5f * solver->N * (p[IX(i,j+1)] - p[IX(i,j-1)]);
-	; END_FOR
-
-	
-	; Siempre agarro 4 conjuntos de 4 pixels, arriba actual abajo, siguiente
-	; comienzo de fila
-	; x x x x - - - - - - - - 
-	; a a a a x x x x - - - -
-	; x x x x - - - - - - - -
-	; - - - - - - - - - - - -
-	; ciclo
-	; - - - - x x x x - - - - 
-	; x x x x a a a a x x x x
-	; - - - - x x x x - - - -
-	; - - - - - - - - - - - -
-	; fin de fila
-	; - - - - - - - - x x x x
-	; - - - - x x x x a a a a
-	; - - - - - - - - x x x x
-	; - - - - - - - - - - - -
-	.segundoCiclo:
-	.principioFilaSC:
-
-	.cicloSC:
-	; COMPLETAR
-	; con fila anterior actual y siguiente sale solo, es directo
-	; pxor xmm0, xmm0
-	; movdqu xmm0, siguiente
-	; movdqu xmm1, anteior
-	; psub xmm0, xmm1 ; tengo la resta de siguiente - anterior para 4 pixels
-	; pongo solver-> N en cada lugar, y multiplico por ceroCinco
-	; multiplico por xmm0 y psub xmmActual, xmm0
-	; solver->u[IX(i,j)] -= 0.5f * solver->N * (p[IX(i+1,j)] - p[IX(i-1,j)]);
-
-	; columna actual y siguiente
-	; actual, arranco en posicion 2, hago las cuentas, hasta la ultima q utiliza a la siguiente.
-	; 	solver->v[IX(i,j)] -= 0.5f * solver->N * (p[IX(i,j+1)] - p[IX(i,j-1)]) 
-	; paso siguiente a actual, y agarro la siguiente, y arranco desde el principio
-	; asi sucesivamente
-	; cuando llego a la ultima columna, voy hasta el 3ero
-
-	.finFilaSC:
-
-	jmp .segundoCiclo
-
-
-
-	; solver_set_bnd ( solver, 1, solver->u );
-	mov rdi, r15
-	mov rsi, 1
-	mov rdx, [rdi + offset_fluid_solver_u]
-	call solver_set_bnd 
-	; solver_set_bnd ( solver, 2, solver->v );
-	mov rdi, r15
-	mov rsi, 1
-	mov rdx, [rdi + offset_fluid_solver_v]
-	call solver_set_bnd 
-
-	pop r15
-	pop r14
-	pop r13
 	pop rbx
 	pop rbp
 	ret
